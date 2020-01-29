c965d50cfe4303a36b906b9e4a31a4dbece954e2
Monil <monilpatel8871@gmail.com>
2020-01-29 16:31:21 -0500

    Added file diceroller.sh

---
+++ bash/diceroller.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# this script rolls a six-sided die
+#
+
+# Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
+
+# roll the dice and display the result
+echo "
+Rolling...
+$(( RANDOM % 6 + 1)) rolled
+"
+
+#  rolling a six sided dice as many letters I have in my first name.
+echo "
+Rolling...
+$(( RANDOM % 6 + 1)) rolled
+"
+
+echo "
+Rolling...
+$(( RANDOM % 6 + 1)) rolled
+"
+
+echo "
+Rolling...
+$(( RANDOM % 6 + 1)) rolled
+"
+
+echo "
+Rolling...
+$(( RANDOM % 6 + 1)) rolled
+"
+
+echo "
+Rolling...
+$(( RANDOM % 6 + 1)) rolled
+"
+
+# this script tells about rolling 5 six-sided dice
+echo "
+Rolling...
+$(( RANDOM % 6 + 1)) rolled $(( RANDOM % 6 + 1)) rolled $(( RANDOM % 6 + 1)) rolled $(( RANDOM % 6 + 1)) rolled $(( RANDOM % 6 + 1)) rolled
+ "
+
+# this script tells about rolling a one  twenty-sided die
+ echo "
+ Rolling...
+$(( RANDOM % 20 + 1))
+"
